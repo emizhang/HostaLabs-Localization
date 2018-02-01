e18fca6afb7e5272f1ba0435b16d4f5dae216a94
Emily Zhang <emilyzhang@dhcp-18-111-63-150.dyn.MIT.EDU>
2018-02-01 15:04:52 -0500

    updated to aws server

--- samples/dnn/mobilenet_ssd_python.py
+++ samples/dnn/mobilenet_ssd_python.py
@@ -31,7 +31,7 @@ WHRatio = inWidth / float(inHeight)
 inScaleFactor = 0.007843
 meanVal = 127.5
 
-r = requests.get("https://radiant-anchorage-55109.herokuapp.com/get_original_image_recent")
+r = requests.get("http://ec2-52-91-230-86.compute-1.amazonaws.com/get_original_image_recent")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -294,10 +294,10 @@ with open('img_data.json', 'w') as outfile:
     json.dump(output, outfile)
 
 #put back onto server here
-res = requests.get("https://radiant-anchorage-55109.herokuapp.com/get_original_image_recent_with_data")
+res = requests.get("http://ec2-52-91-230-86.compute-1.amazonaws.com/get_original_image_recent_with_data")
 data = json.loads(res.text)
 out_data = json.dumps(output)
-r = requests.post("https://radiant-anchorage-55109.herokuapp.com/update_semantic_data", data = { 'filepath' : data["img_data"]["filepath"], 'data' : out_data })
+r = requests.post("http://ec2-52-91-230-86.compute-1.amazonaws.com/update_semantic_data", data = { 'filepath' : data["img_data"]["filepath"], 'data' : out_data })
 print(r)
 
 # img_width, img_height = (original_img.shape[1], original_img.shape[0])
--- server_process_image.sh
+++ server_process_image.sh
@@ -4,7 +4,7 @@
 cd ~/HostaLabs-Localization/samples/dnn/
 
 #saves image from server as "new_name.png in same directory as this file"
-wget https://radiant-anchorage-55109.herokuapp.com/get_original_image_recent -O new_image.png
+wget http://ec2-52-91-230-86.compute-1.amazonaws.com/get_original_image_recent -O new_image.png
 
 #runs python script to get output
 python mobilenet_ssd_python.py --image 'new_image.png' --num_classes 90 --prototxt 'ssd_mobilenet_v1_coco.pbtxt' --weights 'frozen_inference_graph.pb'
